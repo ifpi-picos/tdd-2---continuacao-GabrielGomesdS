d6cf9fae57f267eb05c54243da421655d0f5762d
GabrielGomesdS <capic.2021118tads0297@aluno.ifpi.edu.br>
2023-06-15 21:07:08 -0300

    classe app alterada

--- src/app.js
+++ src/app.js
@@ -6,19 +6,36 @@ const Tarefas = require('./tarefas');
 const tarefas = new Tarefas();
 
 app.post('/tarefas', (req, res) => {
+  const descricao = req.body.descricao;
 
+  if (!descricao) {
+    return res.status(400).json({ error: 'A descrição da tarefa é obrigatória.' });
+  }
+
+  tarefas.adicionar(descricao);
+  res.status(201).send('Tarefa adicionada com sucesso!');
 });
 
 app.delete('/tarefas/:indice', (req, res) => {
+  const indice = parseInt(req.params.indice);
 
+  if (isNaN(indice) || indice < 0 || indice >= tarefas.todas().length) {
+    return res.status(400).json({ error: 'Índice inválido.' });
+  }
+
+  tarefas.remover(indice);
+  res.send('Tarefa removida com sucesso!');
 });
 
 app.get('/tarefas', (req, res) => {
-
+  res.json(tarefas.todas());
 });
 
 app.patch('/tarefas/:indice', (req, res) => {
+  const indice = parseInt(req.params.indice);
 
+  tarefas.marcarConcluida(indice);
+  res.send('Tarefa marcada como concluída!');
 });
 
 module.exports = app;
--- src/tarefas.js
+++ src/tarefas.js
@@ -1,24 +1,31 @@
 class Tarefas {
-    constructor() {
-      this.lista = [];
-    }
-  
-    adicionar(descricao) {
-      
-    }
-  
-    remover(indice) {
-      
-    }
-  
-    todas() {
-      
-    }
-  
-    marcarConcluida(indice) {
-     
+  constructor() {
+    this.lista = [];
+  }
+
+  adicionar(descricao) {
+    const tarefa = {
+      descricao: descricao,
+      concluida: false
+    };
+    this.lista.push(tarefa);
+  }
+
+  remover(indice) {
+    if (indice >= 0 && indice < this.lista.length) {
+      this.lista.splice(indice, 1);
     }
   }
-  
+
+  todas() {
+    return this.lista;
+  }
+
+  marcarConcluida(indice) {
+    if (indice >= 0 && indice < this.lista.length) {
+      this.lista[indice].concluida = true;
+    }
+  }
+}
+
 module.exports = Tarefas;
-  
